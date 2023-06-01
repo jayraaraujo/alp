4c8155349c73458710726c927d5981f6588233ee
jayraaraujo <118615555+jayraaraujo@users.noreply.github.com>
2023-06-01 09:24:43 -0300

    Add files via upload

---
+++ 01.js
@@ -0,0 +1,5 @@
+n = Number(prompt("Digite um número para exibir a tabuada:"));
+for(i=0; i<=10; i++){
+    let m = n * i;
+    document.write(`${n} x ${i} = ${m} <br/>`);
+}                                                                                                                                                                  
